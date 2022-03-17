7b9ebf69360b84bfc3d1d9a00894a057ee9b499b
Elizabeth <ecjohnson891@gmail.com>
2022-03-16 18:20:44 -0600

    axios/react download

---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from 'axios';
+
+export default function Weather() {
+    let city ="Lisbon";
+    return(
+        <div className="App">
+        <h2>Hello from Weather</h2>
+        <br />
+        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+            Shecodes
+        </a>
+        
+        </div>
+    )
+}
