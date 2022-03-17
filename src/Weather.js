e70441968f2829e3cfdfebbed3b7ebcc23209bf5
Elizabeth <ecjohnson891@gmail.com>
2022-03-17 07:48:34 -0600

    took out unused vars

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,8 @@
 import React from "react";
-import axios from 'axios';
-import Loader from 'react-loader-spinner';
+
 
 export default function Weather() {
-    let city ="Lisbon";
+    
     return(
         <div className="App">
         <h2>Hello from Weather</h2>
